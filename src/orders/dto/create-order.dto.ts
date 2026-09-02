69e011314409b1973b3f0178a565f6de75450d8e
HakkaYoro <hackeryolo.hy@gmail.com>
2026-09-02 10:19:46 -0400

    Add CreateOrderDto validation rules (@IsString/@IsNotEmpty/@IsInt/@Min/@Max) + global ValidationPipe. Bootcamp Day 19 - DTOs + Validation.

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,14 @@
-export class CreateOrderDto {}
+import { IsInt, IsNotEmpty, IsString, Max, Min } from "class-validator";
+
+export class CreateOrderDto {
+	@IsString()
+	@IsNotEmpty()
+	cliente: string;
+	@IsString()
+	@IsNotEmpty()
+	item: string;
+	@IsInt()
+	@Min(1)
+	@Max(99)
+	cantidad: number;
+}
